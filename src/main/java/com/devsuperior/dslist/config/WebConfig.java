5b93015f6060e4e304ab3933bd0d6c824c7f931f
abreuwilliam <https://github.com/abreuwilliam>
2025-01-25 13:31:12 -0300

    ATualização de cors

--- src/main/java/com/devsuperior/dslist/config/WebConfig.java
+++ src/main/java/com/devsuperior/dslist/config/WebConfig.java
@@ -5,21 +5,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Configuration
 public class WebConfig {
 
 	@Value("${cors.origins}")
 	private String corsOrigins;
-	
+
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedMethods("*").allowedOrigins("*");
+				// Use a URL específica para o front-end, se necessário
+				registry.addMapping("/**")
+						.allowedMethods("*")
+						.allowedOrigins("http://localhost:5173", "http://localhost:3000", "https://lista-de-jogos-intensivao-nelio-production.up.railway.app");
 			}
 		};
 	}
-	
 }
+
+	
